36ad5892fb6ffec5037a5a22553a234eac09aa76
Koushik Ahmed <koushikxahmed@gmail.com>
2023-11-04 13:26:54 +0600

    updated

---
+++ config.js
@@ -0,0 +1,8 @@
+const PORT = 5555;
+const MONGO_URI =
+  "mongodb+srv://koushik:1234@koushik.khkafvr.mongodb.net/bookStore?retryWrites=true&w=majority";
+
+module.exports = {
+  PORT,
+  MONGO_URI,
+};
